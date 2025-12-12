eae492f0abbb004f353071a80b1c34d8bce9138f
José Neto <jgsn2@aluno.ifal.edu.br>
2025-12-12 11:27:35 -0300

    Inclusão do CASCADE no Model de relacionamentos para permitir exclusão de receitas com avaliações e ajuste de estilização.

--- models/Associacoes.js
+++ models/Associacoes.js
@@ -2,13 +2,13 @@ const Usuario = require('./Usuario');
 const Receita = require('./Receita');
 const Avaliacao = require('./Avaliacao');
 
-Usuario.hasMany(Receita, { foreignKey: 'autorId' });
+Usuario.hasMany(Receita, { foreignKey: 'autorId', onDelete: 'CASCADE' });
 Receita.belongsTo(Usuario, { foreignKey: 'autorId' });
 
-Receita.hasMany(Avaliacao, { foreignKey: 'receitaId' });
+Receita.hasMany(Avaliacao, { foreignKey: 'receitaId', onDelete: 'CASCADE' });
 Avaliacao.belongsTo(Receita, { foreignKey: 'receitaId' });
 
-Usuario.hasMany(Avaliacao, { foreignKey: 'usuarioId' });
+Usuario.hasMany(Avaliacao, { foreignKey: 'usuarioId', onDelete: 'CASCADE' });
 Avaliacao.belongsTo(Usuario, { foreignKey: 'usuarioId' });
 
 module.exports = {
